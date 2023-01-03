95fda177eea1c48776243217b90815b9790d86d5
Maria <maria.carranzavaldes@gmail.com>
2023-01-03 09:25:12 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+
+//Este es el mensaje de bienvenida
+
+console.log('Esta es la puerta de entrada al proyecto')
+
+
+console.log('Hola, este es mi primer ejercicio con Node en el mejor Bootcamp de programación del mundo')
+
+/*Esta es la parte intermedia del proyecto
+En esta parte realizamos los chequeos necesarios 
+Por ultimo vamos a la ultima linea de saludo
+*/
+
+//esta es la despedida
+console.log('Adios')
